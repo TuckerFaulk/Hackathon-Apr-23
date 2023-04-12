b5b7dfbe72d562727581f160633e7f54b53352bc
TuckerFaulk <faulkner1993@hotmail.co.uk>
2023-04-12 10:50:05 +0000

    Update allauth settings and create superuser

--- charlie_charlie_one/settings.py
+++ charlie_charlie_one/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             TEMPLATES_DIR,
-            os.path.join(BASE_DIR, 'templates', 'allauth')
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
