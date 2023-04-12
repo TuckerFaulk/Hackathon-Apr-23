4ce0ed35040a6728d4a3e97872db42bb5eafb7fe
Clinelly <swall289@gmail.com>
2023-04-12 21:00:28 +0000

    Add urls to blog and settings

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import *
 
 # Create your views here.
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).orderby("-created_on")
+    template_name = "post_list.html"
+    paginate_by = 24
