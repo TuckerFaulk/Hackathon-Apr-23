a87fbec814b6af12ee78c6a09294d49d29f1f17d
TuckerFaulk <faulkner1993@hotmail.co.uk>
2023-04-15 08:02:09 +0000

    Remove link to feed. Add and wire up resources page

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Index, AboutUs
+from .views import Index, AboutUs, Resources
 
 
 urlpatterns = [
     path('', Index.as_view(), name='home'),
     path('about-us/', AboutUs.as_view(), name='about-us'),
+    path('resources/', Resources.as_view(), name='resources'),
 ]
