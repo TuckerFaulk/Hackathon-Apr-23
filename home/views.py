d35a80db9b3a91820e4bb67a0bd4b7a200c0c56d
JoyZadan <joyzadan.learning@gmail.com>
2023-04-16 18:03:15 +0000

    Add error handlers and code tidy

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 
 class Index(TemplateView):
@@ -11,3 +12,13 @@ class AboutUs(TemplateView):
 
 class Resources(TemplateView):
     template_name = 'home/resources.html'
+
+
+def handler404(request, exception):
+    """ Custom 404 page """
+    return render(request, "errors/404.html", status=404)
+
+
+def handler500(request):
+    """ Custom 500 page """
+    return render(request, "errors/500.html", status=500)
